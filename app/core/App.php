67425f1d8b96b41e89b8212b76a468193ecc2b96
Dicky Prasetyo Endrianto <dickychigo@gmail.com>
2021-08-28 12:21:17 +0700

    Update App.php

--- app/core/App.php
+++ app/core/App.php
@@ -20,7 +20,7 @@ class App
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
-                $unset($url[1]);
+                unset($url[1]);
             }
         }
 
